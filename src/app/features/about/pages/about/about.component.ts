c8b1481bc6a87daef32e4393d9e7e40679dc8f18
Lily <shirley.fox_@hotmail.com>
2023-10-07 22:44:31 +0200

    Refactor about

--- src/app/features/about/pages/about/about.component.ts
+++ src/app/features/about/pages/about/about.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-
+  readonly skills = [
+    "C", "Python", "Java", "JavaScript / TypeScript", "C#", "Swift", "Kotlin", "PHP",
+    "SQL", "ASP.NET", "Spring Boot", "Laravel", "Angular", "React", "GraphQL", "FastAPI",
+    "Express", "MongoDB", "Node.js", "Git", "Docker"
+  ];
+  readonly experiences = [
+    "Full stack development at FOD Economie",
+    "Teacher assistant at CodeFever"
+  ];
+  readonly educations = [
+    "Associate degree of Programming",
+    "Bachelor of Applied Computer Science (ongoing)"
+  ];
 }
