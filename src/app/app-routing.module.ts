9f465d19677997ab1a5d4575c53e0b984ed7f97a
Lily <shirley.fox_@hotmail.com>
2023-10-07 19:57:52 +0200

    Create about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { HomeRoutingModule } from "./features/home/home-routing.module";
+import { AboutRoutingModule } from "./features/about/about-routing.module";
 
 const routes: Routes = [
   {
     path: "",
     loadChildren: () => HomeRoutingModule
+  },
+  {
+    path: "about",
+    loadChildren: () => AboutRoutingModule
   },
   { path: "**", redirectTo: "/", pathMatch: "full" }
 ];
