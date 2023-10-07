48f44bea5e0619682bf1c981582e429629f896da
Lily <shirley.fox_@hotmail.com>
2023-10-07 22:36:25 +0200

    Change list spacing in about

--- src/app/features/about/about.module.ts
+++ src/app/features/about/about.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './pages/about/about.component';
+import { MatListModule } from "@angular/material/list";
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { AboutComponent } from './pages/about/about.component';
   ],
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    MatListModule
   ]
 })
 export class AboutModule { }
