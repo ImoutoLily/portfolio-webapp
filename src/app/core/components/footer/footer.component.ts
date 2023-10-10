a9d36c5b5b401dd68e36d1c478da24fa172b8e92
Lily <shirley.fox_@hotmail.com>
2023-10-10 14:28:21 +0200

    Add background gradient

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -10,10 +10,12 @@ export class FooterComponent implements OnInit {
   emailAddress: string;
   githubUrl: string;
   linkedInUrl: string;
+  facebookUrl: string;
 
   ngOnInit(): void {
     this.emailAddress = environment.emailAddress;
     this.githubUrl = environment.githubUrl;
     this.linkedInUrl = environment.linkedInUrl;
+    this.facebookUrl = environment.facebookUrl;
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
   githubUsername: "ImoutoLily",
   emailAddress: "lily.vandenbore@gmail.com",
   githubUrl: "https://github.com/ImoutoLily/",
-  linkedInUrl: "https://www.linkedin.com/in/lily-vandenbore-386a6a294/"
+  linkedInUrl: "https://www.linkedin.com/in/lily-vandenbore-386a6a294/",
+  facebookUrl: "https://www.facebook.com/profile.php?id=61551911251406"
 }
